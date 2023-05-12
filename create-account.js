7f8bbcbd073d2a07d80e8ebaf281ab7e5ec591f7
kozaqui <kozaqui@gmail.com>
2023-05-12 12:55:05 +0200

    Add script to generate a single Algorand account

--- create-account.js
+++ create-account.js
@@ -1,11 +1,15 @@
-const { algosdk } = require('./init')
+const { algosdk } = require("./init");
 
 function createAccount() {
-  var account = algosdk.generateAccount()
-  var passphrase = algosdk.secretKeyToMnemonic(account.sk)
+  var account = algosdk.generateAccount();
+  var passphrase = algosdk.secretKeyToMnemonic(account.sk);
   console.log("My address: " + account.addr);
   console.log("My passphrase: " + passphrase);
-  return { account, passphrase }
+  return { account, passphrase };
 }
 
-module.exports = createAccount
+module.exports = createAccount;
+
+if (require.main === module) {
+  createAccount();
+}
